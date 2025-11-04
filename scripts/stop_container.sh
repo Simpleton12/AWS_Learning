201312a5a39ec584885823540af5eb3ddb7e120b
Simpleton12 <86843526+Simpleton12@users.noreply.github.com>
2025-11-04 16:50:53 -0600

    Add debug output for container stopping script
    Added debug echo statements to display container IDs.

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,6 +3,9 @@ set -e
 
 # Stop the running container (if any)
 TEMP=`docker ps | awk -F " " '{print $1}'`
+echo $TEMP
 TEST=`echo $TEMP | awk -F " " '{print $2}'`
+echo $TEST
 
 docker rm -f $TEST
+echo `docker ps`
