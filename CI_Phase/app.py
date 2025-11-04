d841e1804bdbbde3e3305e5f5dd4e86db316f85d
Simpleton12 <86843526+Simpleton12@users.noreply.github.com>
2025-11-04 15:26:26 -0600

    Update greeting message in app.py

--- CI_Phase/app.py
+++ CI_Phase/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Bhanu! - Hope this shit works'
+    return 'Hello, Bhanu! - Hope this shit works - try again'
 
 if __name__ == '__main__':
     app.run()
